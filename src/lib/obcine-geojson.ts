b691ca17605e557ca0cd12dab9d7bd815f9b21e1
mytja <mitja@severkar.eu>
2025-11-28 18:05:34 +0100

    fix?: GeoJSON path

--- src/lib/obcine-geojson.ts
+++ src/lib/obcine-geojson.ts
@@ -1,13 +1,3 @@
-import fs from 'node:fs';
-import { asset } from '$app/paths';
+import assetAsString from '/static/OB.geojson?raw'
 
-export let OBCINE_GEOJSON: any;
-console.log("Loading GeoJSON...");
-fs.readFile(asset('OB.geojson'), 'utf8', (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	OBCINE_GEOJSON = JSON.parse(data);
-	console.log("Loaded GeoJSON!");
-});
+export const OBCINE_GEOJSON = JSON.parse(assetAsString);
